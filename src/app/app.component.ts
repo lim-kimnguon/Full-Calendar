f9e49a81cd27b7b9c8d9d9d1ef07fd51cc55dbcb
lenger0810 <lengerlim0810@gmail.com>
2022-08-23 14:16:42 +0700

    Add event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { CalendarOptions, FullCalendarElement, defineFullCalendarElement } from '@fullcalendar/web-component';
+import { Component } from '@angular/core';
+import { CalendarOptions, defineFullCalendarElement } from '@fullcalendar/web-component';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
 defineFullCalendarElement();
@@ -11,16 +11,37 @@ defineFullCalendarElement();
 })
 export class AppComponent {
   title = 'latest-calendar';
-
-  // references the #calendar in the template
-  @ViewChild('calendar') calendarRef: ElementRef<FullCalendarElement> | undefined;
+  events: any[] = [];
+  users: any[] = [
+    { id: '1', name: 'test1', date: '2022-08-24'},
+    { id: '2', name: 'test2', date: '2022-08-28'}
+  ];
 
   calendarOptions: CalendarOptions = {
     plugins: [dayGridPlugin],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,dayGridWeek,dayGridDay'
+    },
+    initialView: 'dayGridMonth',
+    weekends: true,
   };
 
-  someMethod() {
-    let calendarApi = this.calendarRef!.nativeElement.getApi();
-    calendarApi!.next();
+  onClick() {
+    var date = prompt('Enter your date format(YYYY-MM-DD):');
+    var time = prompt('Enter your time format(HH:MM:SS):');
+    if(!isNaN(Date.parse(date!))) {
+      console.log(new Date(date! + 'T' + time));
+    }
+    this.users.map((item, index) => {
+      this.events.push({ id: `${index + 1}`, title: `Quiz: ${item.name}`, start: date});
+    })
+
+    this.calendarOptions = {
+      ...this.calendarOptions,
+      events: this.events,
+      weekends: false
+    }
   }
 }
